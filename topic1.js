46174e6507b5d1357cac47735e254c1a828d8a4b
vovanolo <vpankiv@softserveinc.com>
2024-03-14 17:31:44 +0200

    topic1

---
+++ topic1.js
@@ -0,0 +1,69 @@
+// alert("Hello guys");
+
+// console.log("Hello guys!"); // вивід повідомлення
+// коментар
+
+// alert("Hello guys");
+// alert("Hello guys");
+// alert("Hello guys");
+
+// змінні
+
+// let var const
+// let const 
+
+// let name = "Volodymyr";
+// let age = 200;
+
+// console.log(name);
+
+// let name = "Volodymyr";
+// let age = 200;
+
+// name = "Ivan";
+
+// age = 100;
+
+// name = 500;
+
+// age = "Ivan";
+
+// console.log(name);
+// console.log(age);
+
+// const age = 200;
+
+
+// age = 100;
+
+// let name = "Volodymyr";
+// name = 10;
+
+// typeof 
+
+// console.log(typeof name);
+// console.log(typeof(name));
+
+
+// alert() prompt() confirm()
+
+// alert("Hello");
+
+// prompt("hello guys");
+
+// let name = prompt("What is your name?", "Volodymyr");
+
+// alert(name);
+// alert("Welcome " + name);
+// alert(`Welcome ${name}`);
+
+
+// console.log(name);
+
+// BigInt
+
+// let bigInt = 243546576587423535n
+// let bigInt = BigInt(243546576587423535);
+
+
+
