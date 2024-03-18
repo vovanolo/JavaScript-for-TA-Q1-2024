3bde6095f1ddd1f988a86ffecf6dcabc6e20549c
vovanolo <vpankiv@softserveinc.com>
2024-03-18 17:40:03 +0200

    topic2

---
+++ topic2.js
@@ -0,0 +1,149 @@
+
+
+// 2 + 2 = 4
+
+// 2 - аргумент, параметр
+
+// +  - оператор 
+
+// + - * / %
+
+// console.log(5 / 5);
+
+// 10 % 2 // 0
+// 10 % 3 // 1
+
+// let x = 10;
+
+// x = x + 1;
+// x += 2;
+// += -= *= \=
+
+// ++ - increment
+
+// let x = 10;
+// x++;
+// ++x;
+
+
+// -- - decrement
+
+// let x = 10;
+// x--;
+
+// Math
+
+// Math.pow(2,2);
+// Math.pow(2,3);
+
+// Math.sqrt(16);
+
+
+// console.log(20 + "10"); //2010
+
+// == - нестрога рівність
+// != нерівність
+// >  < 
+// >=   <=
+
+// console.log(10 == 20);
+// console.log(10 < 20);
+// console.log(10 != 20);
+
+// 0 - false 1.. - true
+// "" - false "sfsf" - true
+
+// let age = +prompt("How old are you?");
+
+// let result = (age > 18) ? "You're an adult!" : "You're not an adult.";
+
+// console.log(result);
+
+// масиви
+
+// [] 
+
+// const students = ["Ivan", "Petro", "John", 324324, null, true];
+// const students = ["Ivan" - 0, "Petro" - 1, "John" - 2];
+
+// const students = ["Ivan", "Petro", "John"];
+
+// console.log(students);
+// console.log(students[1]);
+// students[1] = "Peter";
+
+// students[3] = "Maria";
+
+// students[6] = "Volodymyr";
+
+// console.log(students);
+// console.log(students[5]);
+
+// console.log(students);
+// console.log(students.length);
+
+// students.push("Volodymyr");
+
+// console.log(students);
+
+// if switch
+
+// if(умова){
+//     код
+// }
+
+let age = +prompt("How old are you?");
+
+// if(age > 18){
+//     console.log("age > 18");
+// }
+
+// if(age > 18){
+//     console.log("age > 18");
+// } else {
+//     console.log("your not > 18");
+// }
+
+// if(age > 18){
+//     console.log("age > 18");
+// } else if(age < 18) {
+//     console.log("age < 18");
+// } else{
+//     console.log("youre 18");
+// }
+
+// if(age >= 20 && age < 30){
+//     console.log("age 20-30");
+// }else if(age > 30 && age < 40){
+//     console.log("age 30-40");
+// } else if(age > 40 && age < 50){
+//     console.log("age 40-50");
+// }
+
+// switch(true){
+//     case (age >= 20 && age < 30): // true === true
+//         console.log("age 20-30");
+//         break;
+//     case (age >= 30 && age < 40):
+//             console.log("age 30-40");
+//             break;
+//     case (age >= 40 && age < 50):
+//             console.log("age 40-50");
+//             break;
+// }
+
+
+// switch(age){
+//     case 20: // age === 20
+//         console.log("your 20");
+//         break;
+//     case 30: // age === 30
+//         console.log("your 30");
+//         break;
+//     case 40: // age === 40
+//         console.log("your 40");
+//         break;
+//     default:
+//         console.log('unknown age');
+
+// }
