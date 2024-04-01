b4af6a041e5965144e358cacd9f9fe2569e3c524
vovanolo <vpankiv@softserveinc.com>
2024-04-01 17:20:41 +0300

    topic-06

---
+++ topic6.js
@@ -0,0 +1,89 @@
+// let title = document.getElementById('title');
+
+
+// console.log(title);
+// console.log(title);
+// console.log(title);
+// console.log(title);
+
+// let title = document.getElementsByTagName("h1");
+
+// console.log(title[0]);
+
+// let title = document.getElementsByClassName("title"); 
+
+// console.log(title);
+
+// let title = document.querySelector(".title > .list");
+// let title = document.querySelectorAll(".title");
+
+
+// console.log(title);
+
+// let list = document.querySelector(".list");
+
+// console.log(list);
+// console.log(list.children);
+// console.log(list.children[2]);
+
+// console.log(list.firstElementChild);
+// console.log(list.lastElementChild);
+// console.log(list.lastElementChild.previousElementSibling);
+// console.log(list.firstElementChild.nextElementSibling);
+
+// let list = document.querySelector(".list");
+
+
+// console.log(list.innerHTML);
+// let number = 12313424;
+
+// list.innerHTML = "";
+// list.innerHTML = "Hello";
+// list.innerHTML = "<h1>Hello</h1>";
+// list.innerHTML = `
+// <li>first from js</li>
+// <li>second from js ${number}</li>
+// `
+
+// console.log(list.outerHTML);
+
+// let input = document.querySelector('.inp');
+
+// console.log(input.type);
+
+// input.type = "date";
+
+// input.getAttribute("type");
+// input.setAttribute("data", "1232");
+
+// console.log(input.classList);
+// input.classList.add("newClass");
+// input.classList.toggle("casfsf");
+
+// let title = document.createElement('h1');
+// let container = document.querySelector('.container');
+
+// title.id = "title";
+
+// title.textContent = "Title from js";
+
+// container.appendChild(title);
+
+// container.innerHTML = '<h1 id="title">Title from js</h1>';
+// container.innerHTML += '<h2 id="title">Title from js</h2>'
+
+
+// console.log(title);
+
+// let title = document.querySelector('.title');
+
+
+// title.style.color = 'red';
+// title.style.backgroundColor = 'green';
+
+
+
+
+
+
+
