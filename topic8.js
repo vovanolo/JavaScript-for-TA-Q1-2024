27c62aa12d64b7ddc336b486ed32418bdd0d1b19
vovanolo <vpankiv@softserveinc.com>
2024-04-08 17:30:38 +0300

    topic-08

---
+++ topic8.js
@@ -0,0 +1,27 @@
+// let sum = function(a, b){
+//     return a + b;
+// }
+
+// sum();
+
+
+// let sum = (a,b) =>{
+//     return a + b;
+// }
+
+// let sum = a => a + 1;
+
+
+// let numbers = [1,2,3,4,5,6,7];
+
+// numbers.map(function(element){
+//     console.log(element);
+//     return element + 2;
+// })
+
+// numbers.map((element) => {
+//     console.log(element);
+//     return element + 2;
+// })
+
+// numbers.map((element) => element +2);
