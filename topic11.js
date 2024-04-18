9ac7f998aa1c36c27df7c17b087c663abfcd9ce1
vovanolo <vpankiv@softserveinc.com>
2024-04-18 17:35:39 +0300

    topic11

---
+++ topic11.js
@@ -0,0 +1,23 @@
+// function sum(a, b){
+//     if(a > 0 && b > 0){
+//         return a + b;
+//     } else {
+//         return;
+//     }
+// }
+
+// http://dsfsfs?name=20&surname=30
+
+// api
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//             .then(result => result.json())
+//             .then(result => console.log(result[0].name));
+
+// async function getUsers(){
+//     let result = await fetch("https://jsonplaceholder.typicode.com/users");
+//     let data = await result.json();
+//     console.log(data);
+// }
+
+// getUsers();
